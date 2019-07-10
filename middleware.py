a1b6c22a98f92fbecbe77ff858919c9ae5a120fa
George Zhang <guanghua19@gmail.com>
2019-07-09 21:54:16 -0400

    modify middleware.py and output

--- middleware.py
+++ middleware.py
@@ -19,48 +19,48 @@ the call stack at the point where the exception originally occurred.
 
 class ExceptionMiddleware(object):
     '''The middleware we use.'''
-    
+
     def __init__(self, app):
         self.app = app
-        
+
     def __call__(self, environ, start_response):
         '''call the application can catch exceptions'''
         appiter = None
-        
+
         try:
             appiter = self.app(environ, start_response)
             for item in appiter:
                 yield item
-                
+
         except:
             e_type, e_value, tb = exc_info()
             traceback = ['Traceback (most recent call last):']
             traceback += format_tb(tb)
             traceback.append('%s: %s' % (e_type.__name__, e_value))
-            
+
             try:
                 start_response('500 INTERNAL SERVER ERROR', [
                   ('Content-Type', 'text/plain')] )
             except:
                 pass
-                
-            yield '\n'.join(traceback)
-            
+
+            yield '\n'.join(traceback).encode('utf-8')
+
         if hasattr(appiter, 'close'):
             appiter.close()
-            
-            
+
+
 def hello_world(environ, start_response):
     parameters = parse_qs(environ.get('QUERY_STRING', ''))
     if 'subject' in parameters:
         subject = escape(parameters['subject'][0])
     else:
         subject = 'world'
-       
+
     start_response('200 OK', [('Content-Type', 'text/html')])
     mystr = 'Hello {}'.format(subject)
-    
-    #a = 10 / 0  # to create divide-by-0 exception
+
+    a = 10 / 0  # to create divide-by-0 exception
 
     return [mystr.encode('utf-8')]
 
